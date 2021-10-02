1f9d4d396601f908d1f8c13d6f76e8fe7916c5b8
Rohan Jain <rohanjaincode@gmail.com>
2021-10-02 17:54:46 +0530

    fixed plpayer bugs

--- src/Pages/Playerpage/Playerpage.tsx
+++ src/Pages/Playerpage/Playerpage.tsx
@@ -9,6 +9,8 @@ import { fetchMovieAsync } from '../../redux/player/player.actions';
 import { RootState } from '../../redux/rootReducer';
 import './Playerpage.scss';
 import axios from '../../utils/axiosInstance';
+import SkeletonElement from '../../components/SkeletonElement/SkeletonElement';
+import { useEffect } from 'react';
 
 const Playerpage = () => {
     const dispatch = useDispatch();
@@ -35,42 +37,60 @@ const Playerpage = () => {
         (state: RootState) => state.player
     );
 
-    function setEmomInterval(expr:any, ...rest:any) :any{
-        setTimeout(function() {
+    function setEmomInterval(expr: any, ...rest: any): any {
+        setTimeout(function () {
             expr(...rest);
             setInterval(expr, 60000, ...rest);
         }, 60000 - new Date().getTime() % (60 * 1000));
+    }
+
+    useEffect(() => {
+        if (playerData.data?._id) {
+            setEmomInterval(() => {
+                let rt = localStorage.getItem("runningTime") || 0;
+                let ud = `resume-watch/check-or-update?userId=${localStorage.getItem("user")}&entity=movies&entityId=${playerData.data?._id}&runningTime=${rt}`;
+                axios.get(ud).then(res => {
+                    console.log("done", res.data.runningTime);
+                })
+            });
         }
-        
-    React.useEffect(() => {
-        setMovieSlug(location.pathname.split('/')[2])
-        dispatch(fetchMovieAsync(`/movies/s/${movieSlug}`))
-        console.log("called",playerData);
-        if(playerData?.data?.title){
-            let u=`resume-watch/get-details?userId=${localStorage.getItem("user")}&entity=movies&entityId=${playerData.data?._id}`;
-            axios.get(u).then(res=>{
+    }, [playerData.data?._id])
+
+    useEffect(() => {
+        const slug = location.pathname.split('/')[2]
+        setMovieSlug(slug)
+        dispatch(fetchMovieAsync(`/movies/s/${slug}`))
+    }, [dispatch, location])
+
+    useEffect(() => {
+        if (playerData.data?._id) {
+            let u = `resume-watch/get-details?userId=${localStorage.getItem("user")}&entity=movies&entityId=${playerData.data?._id}`;
+            axios.get(u).then(res => {
                 console.log(res.data.runningTime);
                 setTime(res.data.runningTime);
             })
-            
-            axios.get('movies/views/'+playerData.data?._id).then(res=>{
+        }
+    }, [time, setTime, playerData.data?._id])
+
+    useEffect(() => {
+        if (playerData.data?._id)
+            axios.get('movies/views/' + playerData.data?._id).then(res => {
                 setViews(res.data.views)
             })
-             setEmomInterval(() => {
-                 let rt=localStorage.getItem("runningTime")||0;
-                let ud=`resume-watch/check-or-update?userId=${localStorage.getItem("user")}&entity=movies&entityId=${playerData.data?._id}&runningTime=${rt}`;
-                    axios.get(ud).then(res=>{
-                        console.log("done",res.data.runningTime);
-                })
-             });
-        }
-        
-        
-    }, [dispatch, movieSlug,time, playerData.data?._id])
+    }, [playerData.data?._id])
 
     return (
         <div className="Playerpage">
-            <WatchflixPlayer wtime={time} playerData={playerData} views={views} />
+            {playerData.data ?
+                <WatchflixPlayer currentTime={time} playerData={playerData} views={views} /> :
+                <div className="Skeleton__Player">
+                    <SkeletonElement type="player"></SkeletonElement>
+                    <SkeletonElement type="title"></SkeletonElement>
+                    <SkeletonElement type="text"></SkeletonElement>
+                    <SkeletonElement type="text"></SkeletonElement>
+                    <SkeletonElement type="button"></SkeletonElement>
+                </div>
+            }
             <Slider isLarge={false} title='Recommended Movies' sliderData={recommendedMovies}></Slider>
             <Helmet defer={false}>
                 <title>{playerData?.data?.title || 'Movie'} - {process.env.REACT_APP_NAME}</title>
--- src/components/WatchflixPlayer/WatchflixPlayer.tsx
+++ src/components/WatchflixPlayer/WatchflixPlayer.tsx
@@ -1,12 +1,12 @@
-import { Button, duration } from "@material-ui/core";
-import PlayerContainer from "griffith";
+import { Button } from "@material-ui/core";
+import Player, { ACTIONS, useMessageContextRef } from "griffith";
 import { Movie } from "../../models/movie.interface";
 import { logEvent } from "../../utils/utils";
 import './WatchflixPlayer.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from "../../redux/rootReducer";
 import { addToWatchlistAsync, removeFromWatchlistAsync } from "../../redux/watchlist/watchlist.actions";
-import SkeletonElement from "../SkeletonElement/SkeletonElement";
+import { useEffect } from "react";
 
 interface IWatchflixPlayerProps {
     playerData: {
@@ -15,11 +15,16 @@ interface IWatchflixPlayerProps {
         data: Movie | null;
     };
     views: number;
-    wtime:number;
+    currentTime: number;
 }
 
 const WatchflixPlayer: React.FunctionComponent<IWatchflixPlayerProps> = (props) => {
     const dispatch = useDispatch();
+    const messageContextRef = useMessageContextRef();
+
+    useEffect(() => {
+        messageContextRef.dispatchAction(ACTIONS.TIME_UPDATE, { currentTime: props.currentTime })
+    }, [messageContextRef, props])
 
     const watchlist = useSelector(
         (state: RootState) => state.watchlist
@@ -35,79 +40,67 @@ const WatchflixPlayer: React.FunctionComponent<IWatchflixPlayerProps> = (props) 
         const addedList = watchlist.movies.find((w: any) => w.movie._id === props.playerData.data?._id)
         dispatch(removeFromWatchlistAsync(`/watch-list/${addedList.watchlistId}`, addedList.watchlistId, 'movies'))
     };
-    if (props.playerData.data) {
-        const { title, rated, imdbRating, yearOfRelease, plot, images, videoMain, genres } = (props.playerData.data as Movie)
-        const thumbNail = images[0].location.cloudFrontUrl
-        const autoVideoUrl = videoMain.destinationLocation.location.cloudFrontUrl
-        const sdVideoUrl = autoVideoUrl.split('.').slice(0, -1).join('.') + '_Ott_Hls_Ts_Avc_Aac_16x9_480x270p_0.4Mbps_qvbr.m3u8'
-        const hdVideoUrl = autoVideoUrl.split('.').slice(0, -1).join('.') + '_Ott_Hls_Ts_Avc_Aac_16x9_960x540p_3.5Mbps_qvbr.m3u8'
+    const { title, rated, imdbRating, yearOfRelease, plot, images, videoMain, genres } = (props.playerData.data as Movie)
+    const thumbNail = images[0].location.cloudFrontUrl
+    const autoVideoUrl = videoMain.destinationLocation.location.cloudFrontUrl
+    const sdVideoUrl = autoVideoUrl.split('.').slice(0, -1).join('.') + '_Ott_Hls_Ts_Avc_Aac_16x9_480x270p_0.4Mbps_qvbr.m3u8'
+    const hdVideoUrl = autoVideoUrl.split('.').slice(0, -1).join('.') + '_Ott_Hls_Ts_Avc_Aac_16x9_960x540p_3.5Mbps_qvbr.m3u8'
 
-        const sources = {
-            "auto": {
-                format: 'm3u8',
-                play_url: autoVideoUrl
-            },
-            "sd": {
-                format: 'm3u8',
-                play_url: sdVideoUrl
-            },
-            "hd": {
-                format: 'm3u8',
-                play_url: hdVideoUrl
-            },
+    const sources = {
+        "auto": {
+            format: 'm3u8',
+            play_url: autoVideoUrl
+        },
+        "sd": {
+            format: 'm3u8',
+            play_url: sdVideoUrl
+        },
+        "hd": {
+            format: 'm3u8',
+            play_url: hdVideoUrl
+        },
 
-        }
-
-        const playerProps = {
-            id: 'test-hls-video',
-            title: `${title} - Watchflix`,
-            cover: thumbNail,
-            sources,
-            shouldObserveResize: true,
-            autoplay: true,
-            onEvent: logEvent,
-            elapsed:150,
-            progressDots:[]
-        }
-        
-        return (
-            <div className="WatchflixPlayer">
-                <div className="WatchflixPlayer__Player">
-                    <PlayerContainer {...playerProps}  />
-                </div>
-                <div className="WatchflixPlayer__Info">
-                    <div className="WatchflixPlayer__Info--title">
-                        <h3>{title}</h3>
-                    </div>
-                    <p className="WatchflixPlayer__Info--views">Views: {props.views}</p>
-                    <div className="WatchflixPlayer__Info--genres">
-                        {genres && genres.map(genre => (
-                            <span key={`Genre--id_${genre._id}`} className="genre-title">{genre.title}</span>
-                        ))}
-                        <span className="genre-title">{yearOfRelease}</span>
-                        <span className="genre-title">{rated}</span>
-                        <span className="genre-title">Rating: {imdbRating}</span>
-                    </div>
-                    <p>{plot}</p>
-                </div>
-                <div className="WatchflixPlayer__MovieOptions">
-                    {!hasAddedinWatchlist ?
-                        <Button variant="outlined" color="primary" onClick={handleAdd} >+ Add to my list</Button> :
-                        <Button variant="contained" color="primary" onClick={handleRemove} >- Remove from my list</Button>}
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <div className="Skeleton__Player">
-                <SkeletonElement type="player"></SkeletonElement>
-                <SkeletonElement type="title"></SkeletonElement>
-                <SkeletonElement type="text"></SkeletonElement>
-                <SkeletonElement type="text"></SkeletonElement>
-                <SkeletonElement type="button"></SkeletonElement>
-            </div>
-        )
     }
+
+    const playerProps = {
+        id: 'test-hls-video',
+        title: `${title} - Watchflix`,
+        cover: thumbNail,
+        sources,
+        shouldObserveResize: true,
+        autoplay: true,
+        onEvent: logEvent,
+        elapsed: 150,
+        progressDots: []
+    }
+
+    return (
+        <div className="WatchflixPlayer">
+            <div className="WatchflixPlayer__Player">
+                <Player  {...playerProps} messageContextRef={messageContextRef} />
+            </div>
+            <div className="WatchflixPlayer__Info">
+                <div className="WatchflixPlayer__Info--title">
+                    <h3>{title}</h3>
+                </div>
+                <p className="WatchflixPlayer__Info--views">Views: {props.views}</p>
+                <div className="WatchflixPlayer__Info--genres">
+                    {genres && genres.map(genre => (
+                        <span key={`Genre--id_${genre._id}`} className="genre-title">{genre.title}</span>
+                    ))}
+                    <span className="genre-title">{yearOfRelease}</span>
+                    <span className="genre-title">{rated}</span>
+                    <span className="genre-title">Rating: {imdbRating}</span>
+                </div>
+                <p>{plot}</p>
+            </div>
+            <div className="WatchflixPlayer__MovieOptions">
+                {!hasAddedinWatchlist ?
+                    <Button variant="outlined" color="primary" onClick={handleAdd} >+ Add to my list</Button> :
+                    <Button variant="contained" color="primary" onClick={handleRemove} >- Remove from my list</Button>}
+            </div>
+        </div>
+    )
 }
 
 export default WatchflixPlayer
