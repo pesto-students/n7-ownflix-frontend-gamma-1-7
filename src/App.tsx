2dc2e1065b37d1db4abd98b98bba2f5b5458e396
Rohan Jain <rohanjaincode@gmail.com>
2021-10-08 20:44:57 +0530

    removed popular page and done minor style fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,43 @@
-import { CssBaseline, ThemeProvider } from '@material-ui/core';
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
-import './App.scss';
-import Layout from './hoc/Layout/Layout';
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import "./App.scss";
+import Layout from "./hoc/Layout/Layout";
 import PrivateRoute from "./hoc/Routes/PrivateRoute";
-import Genre from './Pages/Genre/Genre';
-import Homepage from './Pages/Homepage/Homepage';
-import Movies from './Pages/Movies/Movies';
-import Playerpage from './Pages/Playerpage/Playerpage';
-import Popular from './Pages/Popular/Popular';
-import Searchpage from './Pages/Searchpage/Searchpage';
-import Series from './Pages/Series/Series';
-import SeriesGenre from './Pages/SeriesGenre/SeriesGenre';
-import Signin from './Pages/Signin/Signin';
-import Signup from './Pages/Signup/Signup';
-import Verify from './Pages/Verify/Verify';
-import Watchlist from './Pages/Watchlist/Watchlist';
-import { fetchAllGenresAsync } from './redux/genres/genres.actions';
-import { fetchWatchlistAsync } from './redux/watchlist/watchlist.actions';
-import { requests } from './requests';
-import theme from './theme';
-import axios from './utils/axiosInstance';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
-import { RootState } from './redux/rootReducer';
+import Genre from "./Pages/Genre/Genre";
+import Homepage from "./Pages/Homepage/Homepage";
+import Movies from "./Pages/Movies/Movies";
+import Playerpage from "./Pages/Playerpage/Playerpage";
+import Searchpage from "./Pages/Searchpage/Searchpage";
+import Series from "./Pages/Series/Series";
+import SeriesGenre from "./Pages/SeriesGenre/SeriesGenre";
+import Signin from "./Pages/Signin/Signin";
+import Signup from "./Pages/Signup/Signup";
+import Verify from "./Pages/Verify/Verify";
+import Watchlist from "./Pages/Watchlist/Watchlist";
+import { fetchAllGenresAsync } from "./redux/genres/genres.actions";
+import { fetchWatchlistAsync } from "./redux/watchlist/watchlist.actions";
+import { requests } from "./requests";
+import theme from "./theme";
+import axios from "./utils/axiosInstance";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { RootState } from "./redux/rootReducer";
 
 function App() {
-
   const dispatch = useDispatch();
-  const isLoggedIn = useSelector(
-    (state: RootState) => state.auth.isLogin
-  );
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLogin);
   React.useEffect(() => {
-    dispatch(fetchAllGenresAsync(requests.fetchAllGenres))
-  }, [dispatch])
+    dispatch(fetchAllGenresAsync(requests.fetchAllGenres));
+  }, [dispatch]);
   React.useEffect(() => {
     if (isLoggedIn) {
-      dispatch(fetchWatchlistAsync(`/watch-list/user/${localStorage.getItem('user')}`))
+      dispatch(
+        fetchWatchlistAsync(`/watch-list/user/${localStorage.getItem("user")}`)
+      );
     }
-  }, [dispatch, isLoggedIn])
+  }, [dispatch, isLoggedIn]);
   return (
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
@@ -51,13 +49,20 @@ function App() {
           <Route path="/verify/:id" exact component={Verify}></Route>
 
           <Layout>
-            <PrivateRoute path="/watchlist" exact component={Watchlist}></PrivateRoute>
-            <Route path="/popular" exact component={Popular}></Route>
+            <PrivateRoute
+              path="/watchlist"
+              exact
+              component={Watchlist}
+            ></PrivateRoute>
             <Route path="/search" exact component={Searchpage}></Route>
             <Route path="/movies/s/:slug" exact component={Playerpage}></Route>
             <Route path="/movies/:genre" exact component={Genre}></Route>
             <Route path="/movies/" exact component={Movies}></Route>
-            <Route path="/series/s/:slug/:episodeNo" exact component={Playerpage}></Route>
+            <Route
+              path="/series/s/:slug/:episodeNo"
+              exact
+              component={Playerpage}
+            ></Route>
             <Route path="/series/:genre" exact component={SeriesGenre}></Route>
             <Route path="/series/" exact component={Series}></Route>
             <Route path="/" exact component={Homepage}></Route>
@@ -70,18 +75,17 @@ function App() {
   );
 }
 
-if (localStorage.getItem('accessToken')) {
+if (localStorage.getItem("accessToken")) {
   axios
-    .get('/auth/check')
-    .then(async res => {
-      if (res.data.msg === 'Expired') {
+    .get("/auth/check")
+    .then(async (res) => {
+      if (res.data.msg === "Expired") {
         localStorage.clear();
       }
     })
-    .catch(e => {
+    .catch((e) => {
       localStorage.clear();
     });
 }
-
 
 export default App;
--- src/Pages/Movies/Movies.tsx
+++ src/Pages/Movies/Movies.tsx
@@ -1,55 +1,94 @@
-import * as React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Slider from '../../components/Slider/Slider';
-import { fetchPopularMoviesAsync, fetchLatestMoviesAsync, fetchActionMoviesAsync, fetchComedyMoviesAsync, fetchHorrorMoviesAsync, fetchThrillerMoviesAsync } from '../../redux/movies/movies.actions';
-import { RootState } from '../../redux/rootReducer';
-import { requests } from '../../requests';
-import './Movies.scss'
-interface IMoviesProps {
-}
+import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Slider from "../../components/Slider/Slider";
+import {
+  fetchPopularMoviesAsync,
+  fetchLatestMoviesAsync,
+  fetchActionMoviesAsync,
+  fetchComedyMoviesAsync,
+  fetchHorrorMoviesAsync,
+  fetchThrillerMoviesAsync,
+} from "../../redux/movies/movies.actions";
+import { RootState } from "../../redux/rootReducer";
+import { requests } from "../../requests";
+import "./Movies.scss";
+interface IMoviesProps {}
 
 const Movies: React.FunctionComponent<IMoviesProps> = (props) => {
-    const dispatch = useDispatch();
-    const latestMovies = useSelector(
-        (state: RootState) => state.movies.latestMovies
-    );
-    const popularMovies = useSelector(
-        (state: RootState) => state.movies.popularMovies
-    );
-    const actionMovies = useSelector(
-        (state: RootState) => state.movies.actionMovies
-    );
-    // const dramaMovies = useSelector(
-    //     (state: RootState) => state.movies.dramaMovies
-    // );
-    const comedyMovies = useSelector(
-        (state: RootState) => state.movies.comedyMovies
-    );
-    const horrorMovies = useSelector(
-        (state: RootState) => state.movies.horrorMovies
-    );
-    const thrillerMovies = useSelector(
-        (state: RootState) => state.movies.thrillerMovies
-    );
-    React.useEffect(() => {
-        dispatch(fetchPopularMoviesAsync(requests.fetchPopularMovies, 1))
-        dispatch(fetchLatestMoviesAsync(requests.fetchLatestMovies, 1))
-        dispatch(fetchActionMoviesAsync(requests.fetchActionMovies, 1))
-        // dispatch(fetchDramaMoviesAsync(requests.fetchDramaMovies, 1))
-        dispatch(fetchComedyMoviesAsync(requests.fetchComedyMovies, 1))
-        dispatch(fetchHorrorMoviesAsync(requests.fetchHorrorMovies, 1))
-        dispatch(fetchThrillerMoviesAsync(requests.fetchThrillerMovies, 1))
-    }, [dispatch])
-    return (
-        <div className="Movies">
-            <Slider isLarge={false} title='Latest' type="movies" sliderData={latestMovies}></Slider>
-            <Slider isLarge={true} title='Popular' type="movies" sliderData={popularMovies}></Slider>
-            <Slider isLarge={false} title='Action' type="movies" sliderData={actionMovies}></Slider>
-            <Slider isLarge={false} title='Thriller' type="movies" sliderData={thrillerMovies}></Slider>
-            <Slider isLarge={false} title='Comedy' type="movies" sliderData={comedyMovies}></Slider>
-            <Slider isLarge={false} title='Horror' type="movies" sliderData={horrorMovies}></Slider>
-        </div>
-    );
+  const dispatch = useDispatch();
+  const latestMovies = useSelector(
+    (state: RootState) => state.movies.latestMovies
+  );
+  const popularMovies = useSelector(
+    (state: RootState) => state.movies.popularMovies
+  );
+  const actionMovies = useSelector(
+    (state: RootState) => state.movies.actionMovies
+  );
+  // const dramaMovies = useSelector(
+  //     (state: RootState) => state.movies.dramaMovies
+  // );
+  const comedyMovies = useSelector(
+    (state: RootState) => state.movies.comedyMovies
+  );
+  const horrorMovies = useSelector(
+    (state: RootState) => state.movies.horrorMovies
+  );
+  const thrillerMovies = useSelector(
+    (state: RootState) => state.movies.thrillerMovies
+  );
+  React.useEffect(() => {
+    dispatch(fetchPopularMoviesAsync(requests.fetchPopularMovies, 1));
+    dispatch(fetchLatestMoviesAsync(requests.fetchLatestMovies, 1));
+    dispatch(fetchActionMoviesAsync(requests.fetchActionMovies, 1));
+    // dispatch(fetchDramaMoviesAsync(requests.fetchDramaMovies, 1))
+    dispatch(fetchComedyMoviesAsync(requests.fetchComedyMovies, 1));
+    dispatch(fetchHorrorMoviesAsync(requests.fetchHorrorMovies, 1));
+    dispatch(fetchThrillerMoviesAsync(requests.fetchThrillerMovies, 1));
+  }, [dispatch]);
+  return (
+    <div className="Movies">
+      <div className="Series__Header">
+        <h1>Series</h1>
+      </div>
+      <Slider
+        isLarge={false}
+        title="Latest"
+        type="movies"
+        sliderData={latestMovies}
+      ></Slider>
+      <Slider
+        isLarge={true}
+        title="Popular"
+        type="movies"
+        sliderData={popularMovies}
+      ></Slider>
+      <Slider
+        isLarge={false}
+        title="Action"
+        type="movies"
+        sliderData={actionMovies}
+      ></Slider>
+      <Slider
+        isLarge={false}
+        title="Thriller"
+        type="movies"
+        sliderData={thrillerMovies}
+      ></Slider>
+      <Slider
+        isLarge={false}
+        title="Comedy"
+        type="movies"
+        sliderData={comedyMovies}
+      ></Slider>
+      <Slider
+        isLarge={false}
+        title="Horror"
+        type="movies"
+        sliderData={horrorMovies}
+      ></Slider>
+    </div>
+  );
 };
 
 export default Movies;
